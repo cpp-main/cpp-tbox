ebac493405e99e2bfc9f23f677f614110cf5ca4b
Hevake <hevake_lcj@126.com>
2017-05-13 19:18:23 +0800

    1.添加log.h的控制宏；2.定义LOG_MODULE_ID宏

--- base/include/tbox/log.h
+++ base/include/tbox/log.h
@@ -1,3 +1,4 @@
+//! tbox/log.h
 #ifndef TBOX_LOG_H_20170512
 #define TBOX_LOG_H_20170512
 
@@ -11,6 +12,12 @@
 #define LOG_LEVEL_DEBUG     4
 #define LOG_LEVEL_TRACE     5
 
+//! Module ID
+#ifndef LOG_MODULE_ID
+#warning "Please define LOG_MODULE_ID as your module name, otherwise it will be NULL"
+#define LOG_MODULE_ID   NULL
+#endif //LOG_MODULE_ID
+
 //! Define commonly macros
 #define LogPrintf(level, fmt, ...) \
     LogPrintfFunc(LOG_MODULE_ID, __func__, __FILE__, __LINE__, level, fmt, ## __VA_ARGS__)
@@ -19,9 +26,20 @@
 #define LogErr(fmt, ...)    LogPrintf(LOG_LEVEL_ERROR, fmt, ## __VA_ARGS__)
 #define LogWarn(fmt, ...)   LogPrintf(LOG_LEVEL_WARN,  fmt, ## __VA_ARGS__)
 #define LogInfo(fmt, ...)   LogPrintf(LOG_LEVEL_INFO,  fmt, ## __VA_ARGS__)
-#define LogDbg(fmt, ...)    LogPrintf(LOG_LEVEL_DEBUG, fmt, ## __VA_ARGS__)
-#define LogTrace(fmt, ...)  LogPrintf(LOG_LEVEL_TRACE, fmt, ## __VA_ARGS__)
 
+#if !defined(BUILD_LOG_LEVEL) || (BUILD_LOG_LEVEL >= LOG_LEVEL_DEBUG)
+    #define LogDbg(fmt, ...)    LogPrintf(LOG_LEVEL_DEBUG, fmt, ## __VA_ARGS__)
+#else
+    #define LogDbg(fmt, ...)
+#endif
+
+#if !defined(BUILD_LOG_LEVEL) || (BUILD_LOG_LEVEL >= LOG_LEVEL_TRACE)
+    #define LogTrace(fmt, ...)  LogPrintf(LOG_LEVEL_TRACE, fmt, ## __VA_ARGS__)
+#else
+    #define LogTrace(fmt, ...)
+#endif
+
+//!
 //! \brief  Log print function
 //!
 //! \param  module_id   Module Id
@@ -30,6 +48,11 @@
 //! \param  line        Code line
 //! \param  level       Log level
 //! \param  fmt         Log format string
-void LogPrintfFunc(const char *module_id, const char *func_name, const char *file_name, int line, int level, const char *fmt, ...);
+//!
+//! \note   We only declare this function here.
+//!         It's your duty to implement it.
+//!
+void LogPrintfFunc(const char *module_id, const char *func_name, const char *file_name,
+                   int line, int level, const char *fmt, ...);
 
 #endif //TBOX_LOG_H_20170512
