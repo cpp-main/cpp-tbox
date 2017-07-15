fffba7156f79ded3a8267e92ac257791e9c36b5c
Hevake <hevake_lcj@126.com>
2017-07-15 19:21:07 +0800

    实现了Libevent的TimerItem对象，未完待续

---
+++ event/engins/libevent/timer_item.cpp
@@ -0,0 +1,105 @@
+#include "timer_item.h"
+
+#include <cassert>
+#include <event2/event.h>
+
+#include "loop.h"
+
+#include <tbox/log.h>
+
+namespace tbox {
+namespace event {
+
+LibeventTimerItem::LibeventTimerItem(LibeventLoop *wp_loop) :
+    wp_loop_(wp_loop),
+    is_inited_(false)
+{
+    event_assign(&event_, NULL, -1, 0, NULL, NULL);
+}
+
+LibeventTimerItem::~LibeventTimerItem()
+{
+    disable();
+}
+
+bool LibeventTimerItem::initialize(const Timespan &interval, Mode mode)
+{
+    disable();
+
+    interval_ = interval;
+
+    short libevent_events = 0;
+    if (mode == Mode::kPersist)
+        libevent_events = EV_PERSIST;
+
+    int ret = event_assign(&event_, wp_loop_->getEventBasePtr(), -1, libevent_events, LibeventTimerItem::OnEventCallback, this);
+    if (ret == 0) {
+        is_inited_ = true;
+        return true;
+    }
+
+    LogErr("event_assign() fail");
+    return false;
+}
+
+void LibeventTimerItem::setCallback(const CallbackFunc &cb)
+{
+    cb_ = cb;
+}
+
+bool LibeventTimerItem::isEnabled() const
+{
+    if (!is_inited_)
+        return false;
+
+    return event_pending(&event_, EV_TIMEOUT, NULL) != 0;
+}
+
+bool LibeventTimerItem::enable()
+{
+    if (!is_inited_)
+        return false;
+
+    if (isEnabled())
+        return true;
+
+    struct timeval tv = interval_;
+    int ret = event_add(&event_, &tv);
+    if (ret != 0) {
+        LogErr("event_add() fail");
+        return false;
+    }
+
+    return true;
+}
+
+bool LibeventTimerItem::disable()
+{
+    if (!is_inited_)
+        return false;
+
+    if (!isEnabled())
+        return true;
+
+    int ret = event_del(&event_);
+    if (ret != 0) {
+        LogErr("event_del() fail");
+        return false;
+    }
+
+    return true;
+}
+
+void LibeventTimerItem::OnEventCallback(int, short, void *args)
+{
+    LibeventTimerItem *pthis = static_cast<LibeventTimerItem*>(args);
+    pthis->onEvent();
+}
+
+void LibeventTimerItem::onEvent()
+{
+    if (cb_) cb_();
+}
+
+}
+}
---
+++ event/engins/libevent/timer_item.h
@@ -0,0 +1,40 @@
+#ifndef TBOX_EVENT_LIBEVENT_TIMER_ITEM_H_20170715
+#define TBOX_EVENT_LIBEVENT_TIMER_ITEM_H_20170715
+
+#include "../../timer_item.h"
+#include <event2/event_struct.h>
+
+namespace tbox {
+namespace event {
+
+class LibeventLoop;
+
+class LibeventTimerItem : public TimerItem {
+  public:
+    explicit LibeventTimerItem(LibeventLoop *wp_loop);
+    virtual ~LibeventTimerItem();
+
+  public:
+    virtual bool initialize(const Timespan &interval, Mode mode);
+    virtual void setCallback(const CallbackFunc &cb);
+
+    virtual bool isEnabled() const;
+    virtual bool enable();
+    virtual bool disable();
+
+  protected:
+    static void OnEventCallback(int, short, void *args);
+    void onEvent();
+
+  private:
+    LibeventLoop *wp_loop_;
+    struct event event_;
+    bool is_inited_;
+    Timespan interval_;
+    CallbackFunc cb_;
+};
+
+}
+}
+
+#endif //TBOX_EVENT_LIBEVENT_TIMER_ITEM_H_20170715
