f77889ba424b43927a7a3fb5e2bdbe52d80e3860
Hevake <hevake_lcj@126.com>
2017-10-04 09:09:50 +0800

    1.对ThreadPool使用桥接模式进行一次封装，隐藏内部实现; 2.将回调函数改成引用传参

---
+++ eventx/impl/thread_pool.h
@@ -0,0 +1,84 @@
+#ifndef TBOX_THREAD_POOL_IMP_H
+#define TBOX_THREAD_POOL_IMP_H
+
+#include <array>
+#include <map>
+#include <set>
+#include <list>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+#include <tbox/event/forward.h>
+
+namespace tbox {
+namespace eventx {
+namespace impl {
+
+using NonReturnFunc = std::function<void ()>;
+
+/**
+ * 任务项
+ */
+struct TaskItem {
+  int task_id;  //! 任务号
+  NonReturnFunc backend_task;   //! 任务在工作线程中执行函数
+  NonReturnFunc main_cb;        //! 任务执行完成后由main_loop执行的回调函数
+
+  TaskItem(int id, const NonReturnFunc &task, const NonReturnFunc &cb) :
+      task_id(id), backend_task(task), main_cb(cb)
+  { }
+};
+
+/**
+ * 线程池实现
+ */
+class ThreadPool {
+  public:
+    explicit ThreadPool(event::Loop *main_loop);
+    virtual ~ThreadPool();
+
+    bool initialize(int min_thread_num, int max_thread_num);
+
+    int execute(const NonReturnFunc &backend_task, int prio);
+    int execute(const NonReturnFunc &backend_task, const NonReturnFunc &main_cb, int prio);
+
+    int cancel(int task_id);
+
+    void cleanup();
+
+  protected:
+    void threadProc(int id);
+    bool createWorker();
+
+    bool shouldThreadExitWaiting() const;   //! 判定子线程是否需要退出条件变量的wait()函数
+    TaskItem* popOneTask(); //! 取出一个优先级最高的任务
+
+  private:
+    event::Loop *main_loop_ = nullptr; //!< 主线程
+
+    bool is_ready_ = false;     //! 是否已经初始化了
+
+    size_t min_thread_num_ = 0; //!< 最少的线程个数
+    size_t max_thread_num_ = 0; //!< 最多的线程个数
+
+    std::mutex lock_;                //!< 互斥锁
+    std::condition_variable cond_var_;   //!< 条件变量
+
+    std::array<std::list<TaskItem*>, 5> undo_tasks_array_;    //!< 优先级任务列表，5级
+    std::set<int/*task_id*/> doing_tasks_set_;   //!< 记录正在从事的任务
+
+    size_t idle_thread_num_ = 0;    //!< 空间线程个数
+    std::map<int/*thread_id*/, std::thread*> threads_;    //!< 线程对象
+    int thread_id_alloc_ = 0;       //!< 工作线程的ID分配器
+    bool threads_stop_flag_ = false;//!< 是否所有工作线程立即停止标记
+
+    int task_id_alloc_ = 0;         //!< 任务id分配计数器
+};
+
+}
+}
+}
+
+#endif //TBOX_THREAD_POOL_IMP_H
--- eventx/thread_pool.h
+++ eventx/thread_pool.h
@@ -1,81 +1,85 @@
-#ifndef TBOX_THREAD_POOL_H_20171003
-#define TBOX_THREAD_POOL_H_20171003
+#ifndef TBOX_THREAD_POOL_H
+#define TBOX_THREAD_POOL_H
 
-#include <array>
-#include <map>
-#include <set>
-#include <list>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 #include <functional>
-
 #include <tbox/event/forward.h>
 
 namespace tbox {
 namespace eventx {
 
-using NonReturnFunc = std::function<void ()>;
+namespace impl {
+    class ThreadPool;
+}
 
 /**
- * 任务项
- */
-struct TaskItem {
-  int task_id;  //! 任务号
-  NonReturnFunc backend_task;   //! 任务在工作线程中执行函数
-  NonReturnFunc main_cb;        //! 任务执行完成后由main_loop执行的回调函数
-
-  TaskItem(int id, const NonReturnFunc &task, const NonReturnFunc &cb) :
-      task_id(id), backend_task(task), main_cb(cb)
-  { }
-};
-
-/**
- * 线程池实现
+ * 线程池类
  */
 class ThreadPool {
   public:
+    /**
+     * 构造函数
+     *
+     * \param main_loop         主线程的Loop对象指针
+     */
     explicit ThreadPool(event::Loop *main_loop);
     virtual ~ThreadPool();
 
-    bool initialize(int min_thread_num, int max_thread_num);
+    /**
+     * 初始化线程池，指定常驻线程数与最大线程数
+     *
+     * \param min_thread_num    常驻线程数，必须 >=0
+     * \param max_thread_num    最大线程数，必须 >=0，=0表示不限定最大线程数量
+     *
+     * \return bool     是否成功
+     */
+    bool initialize(int min_thread_num = 0, int max_thread_num = 0);
 
-    int execute(NonReturnFunc backend_task, NonReturnFunc main_cb, int prio);
+    using NonReturnFunc = std::function<void ()>;
 
+    /**
+     * 使用worker线程执行某个函数
+     *
+     * \param backend_task      让worker线程执行的函数对象
+     * \param prio              任务优先级[-2, -1, 0, 1, 2]，越小优先级越高
+     *
+     * \return int  <0 任务创建没成功
+     *              >=0 任务id
+     */
+    int execute(const NonReturnFunc &backend_task, int prio = 0);
+
+    /**
+     * 使用worker线程执行某个函数，并在完成之后在主线程执行指定的回调函数
+     *
+     * \param backend_task      让worker线程执行的函数对象
+     * \param main_cb           任务完成后，由主线程执行的回调函数对象
+     * \param prio              任务优先级[-2, -1, 0, 1, 2]，越小优先级越高
+     *
+     * \return int  <0 任务创建没成功
+     *              >=0 任务id
+     */
+    int execute(const NonReturnFunc &backend_task, const NonReturnFunc &main_cb, int prio = 0);
+
+    /**
+     * 取消任务
+     *
+     * \param task_id   任务id
+     *
+     * \return  int     0: 成功
+     *                  1: 没有找到该任务（已执行）
+     *                  2: 该任务正在执行
+     */
     int cancel(int task_id);
 
+    /**
+     * 清理资源，并等待所有的worker线程结束
+     */
     void cleanup();
 
-  protected:
-    void threadProc(int id);
-    bool createWorker();
-
-    bool shouldThreadExitWaiting() const;   //! 判定子线程是否需要退出条件变量的wait()函数
-    TaskItem* popOneTask(); //! 取出一个优先级最高的任务
-
   private:
-    event::Loop *main_loop_ = nullptr; //!< 主线程
-
-    bool is_ready_ = false;     //! 是否已经初始化了
-
-    size_t min_thread_num_ = 0; //!< 最少的线程个数
-    size_t max_thread_num_ = 0; //!< 最多的线程个数
-
-    std::mutex lock_;                //!< 互斥锁
-    std::condition_variable cond_var_;   //!< 条件变量
-
-    std::array<std::list<TaskItem*>, 5> undo_tasks_array_;    //!< 优先级任务列表，5级
-    std::set<int> doing_tasks_set_;   //!< 记录正在从事的任务
-
-    size_t idle_thread_num_ = 0;    //!< 空间线程个数
-    std::map<int, std::thread*> threads_;    //!< 线程对象
-    int thread_id_alloc_ = 0;       //!< 工作线程的ID分配器
-    bool threads_stop_flag_ = false;//!< 是否所有工作线程立即停止标记
-
-    int task_id_alloc_ = 0;         //!< 任务id分配计数器
+    impl::ThreadPool *impl_;
 };
 
 }
 }
 
-#endif //TBOX_THREAD_POOL_H_20171003
+#endif //TBOX_THREAD_POOL_H
