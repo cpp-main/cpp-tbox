8280cc294c54ac6a720cdee16686a81dcf6f970c
Hevake <hevake_lcj@126.com>
2017-10-31 19:31:41 +0800

    完成了BufferedFd的onReadEvent()处理方法

--- network/buffered_fd.cpp
+++ network/buffered_fd.cpp
@@ -154,7 +154,62 @@ bool BufferedFd::send(const void *data_ptr, size_t data_size)
 
 void BufferedFd::onReadCallback(short)
 {
-    LogUndo();
+    struct iovec rbuf[2];
+    char extbuf[1024];  //! 扩展存储空间
+
+    size_t writable_size = recv_buff_.writableSize();
+
+    //! 优先将数据读入到 recv_buff_ 中去，如果它装不下就再存到 extbuff 中
+    rbuf[0].iov_base = recv_buff_.writableBegin();
+    rbuf[0].iov_len  = writable_size;
+    rbuf[1].iov_base = extbuf;
+    rbuf[1].iov_len  = sizeof(extbuf);
+
+    ssize_t rsize = fd_.readv(rbuf, 2);
+    if (rsize > 0) {    //! 读到了数据
+        do {
+            if (static_cast<size_t>(rsize) > writable_size) {
+                //! 如果实际读出的数据比 recv_buff_ 的可写区还大，说明有部分数据是写到了 extbuf 中去了
+                recv_buff_.hasWritten(writable_size);
+                size_t remain_size = rsize - writable_size; //! 计算 extbuf 中的数据大小
+                recv_buff_.append(extbuf, remain_size);
+            } else {
+                recv_buff_.hasWritten(rsize);
+            }
+
+            //! 继续读，直到 rsize == 0，表示读完为止
+            writable_size = recv_buff_.writableSize();
+            rbuf[0].iov_base = recv_buff_.writableBegin();
+            rbuf[0].iov_len  = writable_size;
+        } while ((rsize = fd_.readv(rbuf, 2)) > 0);
+
+        if (recv_buff_.readableSize() >= receive_threshold_) {
+            if (receive_cb_) {
+                ++cb_level_;
+                receive_cb_(recv_buff_);
+                --cb_level_;
+            } else {
+                LogWarn("receive_cb_ is not set");
+                recv_buff_.hasReadAll();    //! 丢弃数据，防止堆积
+            }
+        }
+    } else if (rsize == 0) {    //! 读到0字节数据，说明fd_已不可读了
+        sp_read_event_->disable();
+        if (read_zero_cb_) {
+            ++cb_level_;
+            read_zero_cb_();
+            --cb_level_;
+        }
+    } else {    //! 读出错了
+        if (errno != EAGAIN) {
+            if (error_cb_) {
+                ++cb_level_;
+                error_cb_(errno);
+                --cb_level_;
+            } else
+                LogWarn("read error, rsize:%d, errno:%d, %s", rsize, errno, strerror(errno));
+        }
+    }
 }
 
 void BufferedFd::onWriteCallback(short)
