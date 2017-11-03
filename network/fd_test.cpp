095b341446ad0e67f2ddc00af588716c5b581d8e
Hevake <hevake_lcj@126.com>
2017-11-03 21:50:10 +0800

    针对Fd添加测试用例，并通过测试

--- network/fd_test.cpp
+++ network/fd_test.cpp
@@ -3,6 +3,16 @@
 #include <gtest/gtest.h>
 using namespace tbox::network;
 
+TEST(network_fd, close_func)
+{
+    int closed_fd = -1;
+    {
+        //! 期望它在退出的时候能自动执行对应close函数，并将closed_fd置为12
+        Fd fd(12, [&](int v) { closed_fd = v; });
+    }
+    EXPECT_EQ(closed_fd, 12);
+}
+
 TEST(network_fd, swap)
 {
     Fd fd(12);
@@ -42,3 +52,133 @@ TEST(network_fd, cast)
     Fd fd(12);
     EXPECT_EQ(fd, 12);
 }
+
+TEST(network_fd, copy_construct)
+{
+    int closed_fd = -1;
+    int close_times = 0;
+    {
+        Fd fd1(12, [&](int v) { closed_fd = v; ++close_times; });
+        Fd fd2(fd1);    //! 执行了一个复制构造
+        EXPECT_EQ(close_times, 0);
+        EXPECT_EQ(fd2.get(), 12);
+    }
+    EXPECT_EQ(closed_fd, 12);
+    EXPECT_EQ(close_times, 1);
+}
+
+TEST(network_fd, copy_assign_no_value)
+{
+    int closed_fd = -1;
+    int close_times = 0;
+    {
+        Fd fd1(12, [&](int v) { closed_fd = v; ++close_times; });
+        Fd fd2;
+        fd2 = fd1;  //! 执行了一次复制赋值
+        EXPECT_EQ(close_times, 0);
+        EXPECT_EQ(fd2.get(), 12);
+    }
+    EXPECT_EQ(closed_fd, 12);
+    EXPECT_EQ(close_times, 1);
+}
+
+TEST(network_fd, copy_assign_has_value)
+{
+    int closed_fd = -1;
+    int close_times = 0;
+    {
+        Fd fd1(12, [&](int v) { closed_fd = v; ++close_times; });
+        Fd fd2(13, [&](int v) { closed_fd = v; ++close_times; });
+        fd2 = fd1;  //! 执行了一次复制赋值，13被释放了
+        EXPECT_EQ(close_times, 1);
+        EXPECT_EQ(closed_fd, 13);
+        EXPECT_EQ(fd2.get(), 12);
+    }
+    EXPECT_EQ(closed_fd, 12);
+    EXPECT_EQ(close_times, 2);
+}
+
+TEST(network_fd, move_construct)
+{
+    int closed_fd = -1;
+    int close_times = 0;
+    {
+        Fd fd1(12, [&](int v) { closed_fd = v; ++close_times; });
+        Fd fd2(std::move(fd1)); //! 移动构造
+        EXPECT_EQ(close_times, 0);
+        EXPECT_EQ(fd2.get(), 12);
+        EXPECT_TRUE(fd1.isNull());
+    }
+    EXPECT_EQ(closed_fd, 12);
+    EXPECT_EQ(close_times, 1);
+}
+
+//! 将有值的fd1给到fd2
+TEST(network_fd, move_assign_1)
+{
+    int closed_fd = -1;
+    int close_times = 0;
+    {
+        Fd fd1(12, [&](int v) { closed_fd = v; ++close_times; });
+        Fd fd2;
+        fd2 = std::move(fd1);
+        EXPECT_EQ(close_times, 0);
+        EXPECT_EQ(fd2.get(), 12);
+        EXPECT_TRUE(fd1.isNull());
+    }
+    EXPECT_EQ(closed_fd, 12);
+    EXPECT_EQ(close_times, 1);
+}
+
+//! 将无值的fd1给到有值的fd2
+TEST(network_fd, move_assign_2)
+{
+    int closed_fd = -1;
+    int close_times = 0;
+
+    Fd fd1;
+    Fd fd2(12, [&](int v) { closed_fd = v; ++close_times; });
+    fd2 = std::move(fd1);
+    EXPECT_EQ(close_times, 1);
+    EXPECT_EQ(closed_fd, 12);
+
+    EXPECT_EQ(fd2.get(), -1);
+    EXPECT_TRUE(fd1.isNull());
+    EXPECT_TRUE(fd2.isNull());
+}
+
+//! fd1 与 fd2 都有值，将 fd2 给 fd1
+TEST(network_fd, move_assign_3)
+{
+    int closed_fd = -1;
+    int close_times = 0;
+    {
+        Fd fd1(12, [&](int v) { closed_fd = v; ++close_times; });
+        Fd fd2(13, [&](int v) { closed_fd = v; ++close_times; });
+        fd2 = std::move(fd1);
+        EXPECT_EQ(close_times, 1);
+        EXPECT_EQ(closed_fd, 13);
+        EXPECT_EQ(fd2.get(), 12);
+        EXPECT_TRUE(fd1.isNull());
+    }
+    EXPECT_EQ(closed_fd, 12);
+    EXPECT_EQ(close_times, 2);
+}
+
+//! fd无值fd2有值，将 fd2 给 fd1
+TEST(network_fd, move_assign_4)
+{
+    int closed_fd = -1;
+    int close_times = 0;
+    {
+        Fd fd1;
+        Fd fd2(13, [&](int v) { closed_fd = v; ++close_times; });
+        fd2 = std::move(fd1);
+        EXPECT_EQ(close_times, 1);
+        EXPECT_EQ(closed_fd, 13);
+        EXPECT_TRUE(fd1.isNull());
+        EXPECT_TRUE(fd2.isNull());
+    }
+    EXPECT_EQ(close_times, 1);
+}
+
