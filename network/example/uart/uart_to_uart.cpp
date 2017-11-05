a2e3cf942bbc665a29930966fa1d45476b84b751
Hevake <hevake_lcj@126.com>
2017-11-05 11:42:42 +0800

    为Uart添加了另一个示例程序uart_to_uart，实现两个串口之间的数据连接

---
+++ network/example/uart/uart_to_uart.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <tbox/base/scope_exit.hpp>
+#include <tbox/event/loop.h>
+#include <tbox/event/signal_item.h>
+#include <tbox/network/uart.h>
+
+using namespace std;
+using namespace tbox;
+
+void PrintUsage(const char *proc)
+{
+    cout << "Usage: " << proc << " <dev1> <mode1> <dev2> <mode2>" << endl
+         << "Exp  : " << proc << " /dev/ttyUSB0 '115200 8n1' /dev/ttyUSB1 '19200 8n1'" << endl;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 5) {
+        PrintUsage(argv[0]);
+        return 0;
+    }
+
+    cout << "dev1:" << argv[1] << ", mode1:" << argv[2] << endl;
+    cout << "dev2:" << argv[3] << ", mode2:" << argv[4] << endl;
+
+    auto sp_loop  = event::Loop::New();
+    SetScopeExitAction([=] { delete sp_loop; });
+    auto sp_uart_1  = new network::Uart(sp_loop);
+    SetScopeExitAction([=] { delete sp_uart_1; });
+    auto sp_uart_2  = new network::Uart(sp_loop);
+    SetScopeExitAction([=] { delete sp_uart_2; });
+
+    if (!sp_uart_1->initialize(argv[1]) ||
+        !sp_uart_2->initialize(argv[3]))
+        return 0;
+
+    if (!sp_uart_1->setMode(argv[2]) ||
+        !sp_uart_2->setMode(argv[4])) {
+        PrintUsage(argv[0]);
+        return 0;
+    }
+
+    //! 将两个串口绑定到一起
+    sp_uart_1->bind(sp_uart_2);
+    sp_uart_2->bind(sp_uart_1);
+
+    sp_uart_1->enable();
+    sp_uart_2->enable();
+
+    auto sp_exit = sp_loop->newSignalItem();
+    SetScopeExitAction([=] { delete sp_exit; });
+    sp_exit->initialize(SIGINT, event::Item::Mode::kOneshot);
+    sp_exit->enable();
+    sp_exit->setCallback(
+        [=] {
+            cout << "Info: Exit Loop" << endl;
+            sp_loop->exitLoop();
+        }
+    );
+
+    cout << "Info: Start Loop" << endl;
+    sp_loop->runLoop();
+    cout << "Info: End Loop" << endl;
+
+    return 0;
+}
