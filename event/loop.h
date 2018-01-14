5f6092db637dacbba82f47e7c7c380e9f07eedb9
Hevake <hevake_lcj@126.com>
2018-01-14 16:20:48 +0800

    针对Loop中的runInLoop()与runNext()着重进行说明

--- event/loop.h
+++ event/loop.h
@@ -31,10 +31,25 @@ class Loop {
     virtual bool isInLoopThread() = 0;
 
     using Func = std::function<void()>;
-    //! 注入下一轮将执行的函数，支持跨线程，跨Loop间调用
     virtual void runInLoop(const Func &func) = 0;
-    //! 注入本回调完成后立即执行的函数，不支持跨线程，跨Loop间调用
     virtual void runNext(const Func &func) = 0;
+    /**
+     * runInLoop() 与 runNext() 区别
+     *
+     * runInLoop()
+     *   功能：注入下一轮将执行的函数，有加锁操作，支持跨线程，跨Loop间调用；
+     *   场景：常用于不同Loop之间委派任务或其它线程向Loop线程妥派任务。
+     *
+     * runNext()
+     *   功能：注入本回调完成后立即执行的函数，无加锁操作，不支持跨线程与跨Loop间调用；
+     *   场景：常用于不方便在本函数中执行的操作，比如释放对象自身。
+     *
+     * runInLoop() 能替代 runNext()，但 runNext() 比 runInLoop() 更轻量。
+     *
+     * 建议：
+     *   能用 runNext() 的场景就用 runNext()，不能用再使用 runInLoop()。
+     *   如果你嫌麻烦，也不追求高效，一率采用 runInLoop() 也不会出错。
+     */
 
     virtual FdEvent* newFdEvent() = 0;
     virtual TimerEvent* newTimerEvent() = 0;
