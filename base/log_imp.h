54d1e3e3861be699ec8c8ab6822743649242d410
Hevake <hevake_lcj@126.com>
2018-02-01 13:23:54 +0800

    对日志模块添加注释说明

--- base/log_imp.h
+++ base/log_imp.h
@@ -3,7 +3,7 @@
 
 /**
  * 本文件只声明了函数 LogSetPrintfFunc() 该函数用于指定日志输出函数
- * 如果不指定，则默认不输出。
+ * 如果不指定，则默认不输出日志。
  */
 
 #include <stdarg.h>
@@ -14,18 +14,19 @@ extern "C" {
 
 //! 日志内容
 struct LogContent {
-    const char *module_id;
-    const char *func_name;
-    const char *file_name;
-    int line;
-    int level;
-    const char *fmt;
-    va_list args;
+    const char *module_id;  //!< 模块名
+    const char *func_name;  //!< 函数名
+    const char *file_name;  //!< 文件名
+    int line;               //!< 行号
+    int level;              //!< 日志等级
+    const char *fmt;        //!< 格式串
+    va_list args;           //!< 可变参数
 };
 
+//! 定义日志输出函数
 typedef void (*LogPrintfFuncType)(LogContent *content);
 
-//! \brief  Set log output function
+//! 设置日志输出函数
 void LogSetPrintfFunc(LogPrintfFuncType func);
 
 #ifdef __cplusplus
