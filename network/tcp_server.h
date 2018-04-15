0e8e95960334ea5a53d082b8f96bde6ac0b6cee1
Hevake <hevake_lcj@126.com>
2018-04-15 11:07:54 +0800

    定义了TcpServer的接口，尚未实现

--- network/tcp_server.h
+++ network/tcp_server.h
@@ -4,6 +4,9 @@
 #include <tbox/base/defines.h>
 #include <tbox/event/loop.h>
 
+#include "sockaddr.h"
+#include "buffer.h"
+
 namespace tbox {
 namespace network {
 
@@ -16,6 +19,43 @@ class TcpServer {
     IMMOVABLE(TcpServer);
 
   public:
+    using ConnId = uint64_t;    //! 连接ID号
+
+    //! 客户端ID
+    struct ClientId {
+        size_t pos = 0;
+        ConnId id  = 0;
+
+        bool operator == (const ClientId &rhs) const { return id == rhs.id; }
+        bool operator <  (const ClientId &rhs) const { return id < rhs.id; }
+    };
+
+    //! 设置绑定地址与backlog
+    bool initialize(const SockAddr &bind_addr, int listen_backlog = 0);
+
+    using ConnectedCallback     = std::function<void(const ClientId &)>;
+    using DisconnectedCallback  = std::function<void(const ClientId &)>;
+    using ReceiveCallback       = std::function<void(const ClientId &, Buffer &)>;
+
+    //! 设置有新客户端连接时的回调
+    void setConnectedCallback(const ConnectedCallback &cb);
+    //! 设置有客户端断开时的回调
+    void setDisconnectedCallback(const DisconnectedCallback &cb);
+    //! 设置接收到客户端消息时的回调
+    void setReceiveCallback(const ReceiveCallback &cb, size_t threshold);
+
+    bool start();   //!< 启动服务
+    bool stop();    //!< 停止服务
+    void cleanup(); //!< 清理
+
+    //! 向指定客户端发送数据
+    bool send(const ClientId &client, const void *data_ptr, size_t data_size);
+    //! 断开指定客户端的连接
+    bool disconnect(const ClientId &client);
+
+  private:
+    ConnId last_conn_id_ = 0;
+    //!TODO:
 };
 
 }
