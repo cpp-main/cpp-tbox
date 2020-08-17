854f61a96b7cc9e3b4e58091c6dddca452f7402d
Hevake <hevake_lcj@126.com>
2020-08-17 16:51:19 +0800

    拟定了mqtt::Client的接口

--- mqtt/client.cpp
+++ mqtt/client.cpp
@@ -17,5 +17,58 @@ Client::~Client()
     LogUndo();
 }
 
+bool Client::initialize(const Config &config)
+{
+    LogUndo();
+    return false;
+}
+
+void Client::cleanup()
+{
+    LogUndo();
+}
+
+void Client::setCallbacks(const Callbacks &cbs)
+{
+    LogUndo();
+}
+
+bool Client::start()
+{
+    LogUndo();
+    return false;
+}
+
+bool Client::stop()
+{
+    LogUndo();
+    return false;
+}
+
+int Client::subscribe(const std::string &topic, int *mid, int qos)
+{
+    LogUndo();
+    return 0;
+}
+
+int Client::ubsubscribe(const std::string &topic, int *mid)
+{
+    LogUndo();
+    return 0;
+}
+
+int Client::publish(const std::string &topic, const void *payload_ptr, size_t payload_size,
+                    int qos, bool retain, int *mid)
+{
+    LogUndo();
+    return 0;
+}
+
+bool Client::isConnected() const
+{
+    LogUndo();
+    return false;
+}
+
 }
 }
--- mqtt/client.h
+++ mqtt/client.h
@@ -1,6 +1,8 @@
 #ifndef TBOX_MQTT_CLIENT_H_20200815
 #define TBOX_MQTT_CLIENT_H_20200815
 
+#include <tbox/base/defines.h>
+#include <tbox/base/memblock.h>
 #include <tbox/event/loop.h>
 
 namespace tbox {
@@ -10,6 +12,77 @@ class Client {
   public:
     explicit Client(event::Loop *wp_loop);
     virtual ~Client();
+
+    NONCOPYABLE(Client);
+    IMMOVABLE(Client);
+
+  public:
+    //! 配置项
+    struct Config {
+        struct Basic {
+            struct Broker {
+                std::string domain = "localhost";
+                uint16_t port = 1883;
+            };
+            Broker broker;
+            std::string client_id;
+            std::string username, passwd;
+            int keepalive = 60;
+        };
+        struct TLS {
+            std::string ca_file, ca_path;
+            std::string cert_file;
+            std::string key_file;
+            bool is_require_peer_cert = true;
+            std::string ssl_version;
+            bool is_insecure = false;
+        };
+        struct Will {
+            std::string topic;
+            Memblock payload;
+            int qos = 0;
+            bool retain = false;
+        };
+
+        Basic basic;
+        Will  will;
+        TLS   tls;
+    };
+
+    bool initialize(const Config &config);
+    void cleanup();
+
+    using ConnectedCallback         = std::function<void()>;
+    using DisconnectedCallback      = std::function<void()>;
+    using MessageReceivedCallback   = std::function<void(int /*mid*/, const std::string &/*topic*/,
+                                                         const void* /*payload_ptr*/, int/*payload_size*/,
+                                                         int /*qos*/, bool /*retain*/)>;
+    using MessagePublishedCallback  = std::function<void(int /*mid*/)>;
+    using TopicSubscribedCallback   = std::function<void(int /*mid*/, int /*qos*/, const int* /*granted_qos*/)>;
+    using TopicUnsubscribedCallback = std::function<void(int /*mid*/)>;
+
+    //! 回调函数
+    struct Callbacks {
+        ConnectedCallback           connected;
+        DisconnectedCallback        disconnected;
+        MessageReceivedCallback     message_recv;
+        MessagePublishedCallback    message_pub;
+        TopicSubscribedCallback     subscribed;
+        TopicUnsubscribedCallback   unsubscribed;
+    };
+
+    void setCallbacks(const Callbacks &cbs);
+
+    bool start();
+    bool stop();
+
+    int subscribe(const std::string &topic, int *mid = nullptr, int qos = 0);
+    int ubsubscribe(const std::string &topic, int *mid = nullptr);
+    int publish(const std::string &topic,
+                const void *payload_ptr = nullptr, size_t payload_size = 0,
+                int qos = 0, bool retain = false, int *mid = nullptr);
+
+    bool isConnected() const;
 
   private:
     struct Data;
