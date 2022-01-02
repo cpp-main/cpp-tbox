9e8df30b7ec4c5a6e02c23a424e213a184d780d2
Hevake <hevake_lcj@126.com>
2022-01-02 15:02:49 +0800

    完善main： 1.完善Args中的help打印; 2.优化版本显示.

--- main/args.cpp
+++ main/args.cpp
@@ -41,6 +41,7 @@ bool Args::parse(int argc, char **argv)
 
         switch (flag) {
             case 'h':
+            case '?':   //! 如果参数不合法
                 printHelp(argv[0]);
                 run = false;
                 break;
@@ -76,7 +77,28 @@ bool Args::parse(int argc, char **argv)
 
 void Args::printHelp(const char *proc_name)
 {
-    //TODO:
+    cout << "Usage: " << proc_name << " [OPTION]" << endl
+        << GetAppDescribe() << endl << endl
+        << "OPTION:" << endl
+        << "  -h, --help            display this help text and exit" << endl
+        << "  -v, --version         display version and exit" << endl
+        << "  -p, --print           display config" << endl
+        << "  -n, --not_run         not run and exit" << endl
+        << "  -s, --set=TEXT        set config filed" << endl
+        << "  -c, --config=FILE     specify config file, file context type: JSON" << endl
+        << endl
+        << "EXAMPLE:" << endl
+        << "  " << proc_name << endl
+        << "  " << proc_name << R"( -c somewhere/conf.json)" << endl
+        << "  " << proc_name << R"( -c somewhere/conf.json -p)" << endl
+        << "  " << proc_name << R"( -c somewhere/conf.json -pn)" << endl
+        << "  " << proc_name << R"( -s 'log.level=6' -s 'log.output="stdout"')" << endl
+        << "  " << proc_name << R"( -s 'log={"level":5,"output":"tcp","tcp":{"ip":"192.168.0.20","port":50000}}')" << endl
+        << "  " << proc_name << R"( -c somewhere/conf.json -s 'log.level=6' -s 'thread_pool.min_thread=2')" << endl
+        << endl
+        << "CONFIG:" << endl
+        << R"(  type ")" << proc_name << R"( -pn" to display default config.)" << endl
+        << endl;
 }
 
 void Args::printVersion()
@@ -84,17 +106,19 @@ void Args::printVersion()
     int major, minor, rev, build;
     GetAppVersion(major, minor, rev, build);
 
-    cout << "version: " << major << '.' << minor << '.' << rev << '_' << build << endl
-         << "buid: " << GetAppBuildTime() << endl;
+    cout << "version : " << major << '.' << minor << '.' << rev << '_' << build << endl
+         << "   buid : " << GetAppBuildTime() << endl;
 }
 
 void Args::loadConfig(const char *config_filename)
 {
+    cout << "loadConfig(" << config_filename << ")" << endl;
     //TODO:
 }
 
 void Args::set(const char *set_string)
 {
+    cout << "set(" << set_string << ")" << endl;
     //TODO:
 }
 
