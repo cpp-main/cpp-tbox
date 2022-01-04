d9c170e945597a2eb8d1571de145def519960036
Hevake <hevake_lcj@126.com>
2022-01-04 08:10:50 +0800

    完善main中的Args，实现了 -s 参数的解释

--- main/args.cpp
+++ main/args.cpp
@@ -97,7 +97,7 @@ void Args::printHelp(const std::string &proc_name)
         << "  -c FILE           specify config file, file type: JSON" << endl
         << "  -s KEY=VALUE      set config field" << endl
         << "  -p                display config" << endl
-        << "  -n                not run and exit" << endl
+        << "  -n                not run" << endl
         << endl
         << "EXAMPLE:" << endl
         << "  " << proc_name << endl
@@ -141,15 +141,59 @@ bool Args::load(const std::string &config_filename)
     return true;
 }
 
+/**
+ * 根据 key 生成对应的 Json 补丁对象，并返回后终的 Json 对象引用
+ *
+ * 如:
+ *  key 为 "x.y.z" 时，则生成 Json 对象：
+ *  {
+ *    "x":{
+ *      "y":{
+ *        "z":{}
+ *      }
+ *    }
+ *  }
+ *  并返回 z 的 Json 对象引用
+ */
+Json& BuildJsonByKey(const std::string &key, Json &js_root)
+{
+    vector<string> str_vec;
+    util::string::Split(key, ".", str_vec);
+
+    Json *p_node = &js_root;
+    //! 为什么取地址，而不是引用？
+    //! 因为 Json::operator= 被重写了的，js_node = js_node[str] 会覆盖原内容
+    for (auto str : str_vec)
+        p_node = &((*p_node)[str]);
+
+    return *p_node;
+}
+
 bool Args::set(const std::string &set_string)
 {
-    cout << "set(" << set_string << ")" << endl;
     vector<string> str_vec;
     if (util::string::Split(set_string, "=", str_vec) != 2) {
         cerr << "Error: in -s --set option, TEXT format is '<KEY>=<VALUE>'" << endl;
         return false;
     }
+    std::string key   = util::string::Strip(str_vec[0]);
+    std::string value = util::string::Strip(str_vec[1]);
 
+    if (key.empty() || value.empty()) {
+        cerr << "Error: key or value is empty";
+        return false;
+    }
+
+    Json js_patch = Json::object();
+    try {
+        Json &js_node = BuildJsonByKey(key, js_patch);
+        js_node = Json::parse(value);
+    } catch (const exception &e) {
+        cerr << "Error: parse KEY=VALUE fail, " << e.what() << endl;
+        return false;
+    }
+
+    conf_.merge_patch(js_patch);
     return true;
 }
 
