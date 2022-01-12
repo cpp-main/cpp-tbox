193aa5985c2b28d7814641b67b5beabac4fd84e4
Hevake <hevake_lcj@126.com>
2022-01-12 08:37:04 +0800

    Further modify the Makefile so that the header files are not restricted by subdirectories when copied

--- tools/lib_common.mk
+++ tools/lib_common.mk
@@ -5,7 +5,7 @@
 #
 # HOW TO USE:
 # before include this file. those variables need be specified.
-# LIB_VERSION_X, LIB_VERSION_Y, LIB_VERSION_Z, SRC_FILES, CXXFLAGS, TEST_LDFLAGS
+# LIB_VERSION_X, LIB_VERSION_Y, LIB_VERSION_Z, C_SRC_FILES, CC_SRC_FILES, CPP_SRC_FILES, CXXFLAGS, TEST_LDFLAGS
 #
 # TARGETS:
 # all, libxxx.a, libxxx.so.x.x.x, test, clean, distclean, install, uninstall
@@ -44,9 +44,12 @@ print_vars:
 ################################################################
 
 CPP_SOURCE_TO_OBJECT = $(LIB_OUTPUT_DIR)/$(subst .cpp,.o,$(1))
+CC_SOURCE_TO_OBJECT = $(LIB_OUTPUT_DIR)/$(subst .cc,.o,$(1))
 C_SOURCE_TO_OBJECT = $(LIB_OUTPUT_DIR)/$(subst .c,.o,$(1))
 
-OBJECTS := $(foreach src,$(SRC_FILES),$(call CPP_SOURCE_TO_OBJECT,$(src)))
+OBJECTS := $(foreach src,$(CPP_SRC_FILES),$(call CPP_SOURCE_TO_OBJECT,$(src)))
+OBJECTS += $(foreach src,$(CC_SRC_FILES),$(call CC_SOURCE_TO_OBJECT,$(src)))
+OBJECTS += $(foreach src,$(C_SRC_FILES),$(call C_SOURCE_TO_OBJECT,$(src)))
 
 define CREATE_CPP_OBJECT
 $(call CPP_SOURCE_TO_OBJECT,$(1)) : $(1)
@@ -55,13 +58,22 @@ $(call CPP_SOURCE_TO_OBJECT,$(1)) : $(1)
 	@$(CXX) $(CXXFLAGS) -o $$@ -c $$^
 endef
 
+define CREATE_CC_OBJECT
+$(call CC_SOURCE_TO_OBJECT,$(1)) : $(1)
+	@echo "\033[32mCXX $$^\033[0m"
+	@mkdir -p $$(dir $$@)
+	@$(CXX) $(CXXFLAGS) -o $$@ -c $$^
+endef
+
 define CREATE_C_OBJECT
 $(call C_SOURCE_TO_OBJECT,$(1)) : $(1)
 	@echo "\033[32mCXX $^\033[0m"
-	@$(CC) $(CXXFLAGS) -o $@ -c $^
+	@$(CC) $(CFLAGS) -o $@ -c $^
 endef
 
-$(foreach src,$(SRC_FILES),$(eval $(call CREATE_CPP_OBJECT,$(src))))
+$(foreach src,$(CPP_SRC_FILES),$(eval $(call CREATE_CPP_OBJECT,$(src))))
+$(foreach src,$(CC_SRC_FILES),$(eval $(call CREATE_CC_OBJECT,$(src))))
+$(foreach src,$(C_SRC_FILES),$(eval $(call CREATE_C_OBJECT,$(src))))
 
 print_static_vars :
 	@echo CXXFLAGS=$(CXXFLAGS)
@@ -78,10 +90,15 @@ $(LIB_OUTPUT_DIR)/$(STATIC_LIB) : print_static_vars $(OBJECTS)
 ################################################################
 
 CPP_SOURCE_TO_SHARED_OBJECT = $(LIB_OUTPUT_DIR)/$(subst .cpp,.oS,$(1))
+CC_SOURCE_TO_SHARED_OBJECT = $(LIB_OUTPUT_DIR)/$(subst .cc,.oS,$(1))
 C_SOURCE_TO_SHARED_OBJECT = $(LIB_OUTPUT_DIR)/$(subst .c,.oS,$(1))
 
-SHARED_OBJECTS := $(foreach src,$(SRC_FILES),$(call CPP_SOURCE_TO_SHARED_OBJECT,$(src)))
+SHARED_OBJECTS := $(foreach src,$(CPP_SRC_FILES),$(call CPP_SOURCE_TO_SHARED_OBJECT,$(src)))
+SHARED_OBJECTS += $(foreach src,$(CC_SRC_FILES),$(call CC_SOURCE_TO_SHARED_OBJECT,$(src)))
+SHARED_OBJECTS += $(foreach src,$(C_SRC_FILES),$(call C_SOURCE_TO_SHARED_OBJECT,$(src)))
+
 SHARED_CXXFLAGS := $(CXXFLAGS) -fPIC
+SHARED_CFLAGS := $(CFLAGS) -fPIC
 
 define CREATE_CPP_SHARED_OBJECT
 $(call CPP_SOURCE_TO_SHARED_OBJECT,$(1)) : $(1)
@@ -90,13 +107,22 @@ $(call CPP_SOURCE_TO_SHARED_OBJECT,$(1)) : $(1)
 	@$(CXX) $(SHARED_CXXFLAGS) -o $$@ -c $$^
 endef
 
+define CREATE_CC_SHARED_OBJECT
+$(call CC_SOURCE_TO_SHARED_OBJECT,$(1)) : $(1)
+	@echo "\033[32mCXX $$^\033[0m"
+	@mkdir -p $$(dir $$@)
+	@$(CXX) $(SHARED_CXXFLAGS) -o $$@ -c $$^
+endef
+
 define CREATE_C_SHARED_OBJECT
 $(call C_SOURCE_TO_SHARED_OBJECT,$(1)) : $(1)
 	@echo "\033[32mCXX $^\033[0m"
-	@$(CC) $(SHARED_CXXFLAGS) -o $@ -c $^
+	@$(CC) $(SHARED_CFLAGS) -o $@ -c $^
 endef
 
-$(foreach src,$(SRC_FILES),$(eval $(call CREATE_CPP_SHARED_OBJECT,$(src))))
+$(foreach src,$(CPP_SRC_FILES),$(eval $(call CREATE_CPP_SHARED_OBJECT,$(src))))
+$(foreach src,$(CC_SRC_FILES),$(eval $(call CREATE_CC_SHARED_OBJECT,$(src))))
+$(foreach src,$(C_SRC_FILES),$(eval $(call CREATE_C_SHARED_OBJECT,$(src))))
 
 print_shared_vars :
 	@echo SHARED_LIB=$(SHARED_LIB)
@@ -118,7 +144,7 @@ $(LIB_OUTPUT_DIR)/$(SHARED_LIB) : print_shared_vars $(SHARED_OBJECTS)
 CPP_SOURCE_TO_TEST_OBJECT = $(LIB_OUTPUT_DIR)/$(subst .cpp,.oT,$(1))
 C_SOURCE_TO_TEST_OBJECT = $(LIB_OUTPUT_DIR)/$(subst .c,.oT,$(1))
 
-TEST_OBJECTS := $(foreach src,$(TEST_SRC_FILES),$(call CPP_SOURCE_TO_TEST_OBJECT,$(src)))
+TEST_OBJECTS := $(foreach src,$(TEST_CPP_SRC_FILES),$(call CPP_SOURCE_TO_TEST_OBJECT,$(src)))
 TEST_CXXFLAGS := $(CXXFLAGS)
 
 define CREATE_CPP_TEST_OBJECT
@@ -128,13 +154,7 @@ $(call CPP_SOURCE_TO_TEST_OBJECT,$(1)) : $(1)
 	@$(CXX) $(TEST_CXXFLAGS) -o $$@ -c $$^
 endef
 
-define CREATE_C_TEST_OBJECT
-$(call C_SOURCE_TO_TEST_OBJECT,$(1)) : $(1)
-	@echo "\033[32mCXX $^\033[0m"
-	@$(CC) $(TEST_CXXFLAGS) -o $@ -c $^
-endef
-
-$(foreach src,$(TEST_SRC_FILES),$(eval $(call CREATE_CPP_TEST_OBJECT,$(src))))
+$(foreach src,$(TEST_CPP_SRC_FILES),$(eval $(call CREATE_CPP_TEST_OBJECT,$(src))))
 
 test : $(LIB_OUTPUT_DIR)/test
 
@@ -147,21 +167,31 @@ $(LIB_OUTPUT_DIR)/test: print_test_vars $(TEST_OBJECTS) $(OBJECTS)
 	@echo "\033[35mBUILD test\033[0m"
 	@$(CXX) -o $@ $(TEST_OBJECTS) $(OBJECTS) $(TEST_LDFLAGS) -lgmock_main -lgmock -lgtest -lpthread
 
+# head files
+HEAD_TO_DEST_FILE = $(addprefix $(STAGING_DIR)/include/tbox/$(LIB_NAME)/,$(1))
+
+define CREATE_HEAD_FILE_TARGET
+$(call HEAD_TO_DEST_FILE,$(1)) : $(1)
+	@mkdir -p $$(dir $$@)
+	@echo "\033[35mCOPY $(1)\033[0m"
+	@cp $$^ $$@
+endef
+
+$(foreach src,$(HEAD_FILES),$(eval $(call CREATE_HEAD_FILE_TARGET,$(src))))
+
+HEAD_FILE_TARGETS := $(foreach src,$(HEAD_FILES),$(call HEAD_TO_DEST_FILE,$(src)))
+
 ################################################################
 # clean and distclean
 ################################################################
 clean:
 	-rm -f $(OBJECTS) $(SHARED_OBJECTS) $(TEST_OBJECTS)
 
-distclean: clean
-	-rm -f $(LIB_BASENAME).* test
-
 ################################################################
 # install and uninstall
 ################################################################
-install:
-	mkdir -p $(STAGING_DIR)/include/tbox/$(LIB_NAME)
-	cp -dr $(HEAD_FILES) $(STAGING_DIR)/include/tbox/$(LIB_NAME)
+
+install: $(HEAD_FILE_TARGETS)
 ifeq ($(ENABLE_STATIC_LIB),yes)
 	cp $(LIB_OUTPUT_DIR)/$(STATIC_LIB) $(STAGING_DIR)/lib/
 endif
