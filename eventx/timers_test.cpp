2497dd5c0ba8d13641bd4b7a64a4ded952a9d6e8
Hevake <hevake_lcj@126.com>
2022-01-20 23:21:55 +0800

    achived Timers unit test

--- eventx/timers_test.cpp
+++ eventx/timers_test.cpp
@@ -12,6 +12,10 @@ using namespace tbox::eventx;
 
 TEST(Timers, doEvery)
 {
+}
+
+TEST(Timers, doAfter)
+{
     Loop *sp_loop = event::Loop::New();
     Timers timers(sp_loop);
     SetScopeExitAction([sp_loop]{ delete sp_loop;});
@@ -32,15 +36,46 @@ TEST(Timers, doEvery)
     timers.cleanup();
 }
 
-TEST(Timers, doAfter)
-{
-}
-
 TEST(Timers, cancel)
 {
+    Loop *sp_loop = event::Loop::New();
+    Timers timers(sp_loop);
+    SetScopeExitAction([sp_loop]{ delete sp_loop;});
+
+    bool tag = false;
+    auto token = timers.doAfter(milliseconds(100),
+        [&] (const Timers::Token &){
+            tag = true;
+        }
+    );
+    timers.cancel(token);
+    sp_loop->exitLoop(chrono::milliseconds(200));
+    sp_loop->runLoop();
+
+    timers.cleanup();
+
+    EXPECT_FALSE(tag);
 }
 
 TEST(Timers, doAt)
 {
+    Loop *sp_loop = event::Loop::New();
+    Timers timers(sp_loop);
+    SetScopeExitAction([sp_loop]{ delete sp_loop;});
+
+    auto start_time = system_clock::now();
+    Timers::Token token;
+    token = timers.doAt(start_time + milliseconds(1000),
+        [&] (const Timers::Token &t){
+            EXPECT_EQ(t, token);
+            auto d = system_clock::now() - start_time;
+            EXPECT_GT(d, milliseconds(995));
+            EXPECT_LT(d, milliseconds(1005));
+        }
+    );
+    sp_loop->exitLoop(chrono::milliseconds(1500));
+    sp_loop->runLoop();
+
+    timers.cleanup();
 }
 
