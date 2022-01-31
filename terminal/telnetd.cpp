c572d8d3c4105c8dad6ca6cec4dd8c1b27b1092a
Hevake <hevake_lcj@126.com>
2022-01-31 22:02:10 +0800

    给Telnetd添加代码

--- terminal/telnetd.cpp
+++ terminal/telnetd.cpp
@@ -27,15 +27,11 @@ class Telnetd::Impl : public Connection {
     void cleanup();
 
   public:
-    bool send(const SessionToken &session, const std::string &str) override;
-    bool endSession(const SessionToken &session) override;
-    bool isValid(const SessionToken &session) const override;
+    bool send(const SessionToken &st, const std::string &str) override;
+    bool endSession(const SessionToken &st) override;
+    bool isValid(const SessionToken &st) const override;
 
   protected:
-    void onTcpConnected(const TcpServer::ClientToken &client);
-    void onTcpDisconnected(const TcpServer::ClientToken &client);
-    void onTcpReceived(const TcpServer::ClientToken &client, Buffer &buff);
-
     enum Cmd {
         kEOF = 236,
         kSUSP,
@@ -65,16 +61,31 @@ class Telnetd::Impl : public Connection {
         kSPEED = 32,
     };
 
-    bool send(const TcpServer::ClientToken &token, const void *data_ptr, size_t data_size);
+    bool send(const TcpServer::ClientToken &ct, const void *data_ptr, size_t data_size);
 
-    void sendWill(const TcpServer::ClientToken &client, Opt opt);
-    void sendWont(const TcpServer::ClientToken &client, Opt opt);
-    void sendDo(const TcpServer::ClientToken &client, Opt opt);
-    void sendDont(const TcpServer::ClientToken &client, Opt opt);
+    void sendString(const TcpServer::ClientToken &ct, const std::string &str);
 
-    void sendCmd(const TcpServer::ClientToken &client, Cmd cmd);
+    void sendWill(const TcpServer::ClientToken &ct, Opt opt);
+    void sendWont(const TcpServer::ClientToken &ct, Opt opt);
+    void sendDo(const TcpServer::ClientToken &ct, Opt opt);
+    void sendDont(const TcpServer::ClientToken &ct, Opt opt);
 
-    void sendSub(const TcpServer::ClientToken &token, Opt o, const uint8_t *p, size_t s);
+    void sendCmd(const TcpServer::ClientToken &ct, Cmd cmd);
+    void sendSub(const TcpServer::ClientToken &ct, Opt o, const uint8_t *p, size_t s);
+
+    void onTcpConnected(const TcpServer::ClientToken &ct);
+    void onTcpDisconnected(const TcpServer::ClientToken &ct);
+    void onTcpReceived(const TcpServer::ClientToken &ct, Buffer &buff);
+
+    void onRecvString(const TcpServer::ClientToken &ct, const std::string &str);
+
+    void onRecvWill(const TcpServer::ClientToken &ct, Opt opt);
+    void onRecvWont(const TcpServer::ClientToken &ct, Opt opt);
+    void onRecvDo(const TcpServer::ClientToken &ct, Opt opt);
+    void onRecvDont(const TcpServer::ClientToken &ct, Opt opt);
+
+    void onRecvCmd(const TcpServer::ClientToken &ct, Cmd cmd);
+    void onRecvSub(const TcpServer::ClientToken &ct, Opt opt, const uint8_t *p, size_t s);
 
   private:
     Loop *wp_loop_ = nullptr;
@@ -158,106 +169,111 @@ void Telnetd::Impl::cleanup()
     sp_tcp_->cleanup();
 }
 
-bool Telnetd::Impl::send(const SessionToken &session, const std::string &str)
+bool Telnetd::Impl::send(const SessionToken &st, const std::string &str)
 {
-    auto client = session_to_client_.at(session);
-    if (session.isNull())
+    auto ct = session_to_client_.at(st);
+    if (st.isNull())
         return false;
 
-    sp_tcp_->send(client, str.c_str(), str.size());
+    sp_tcp_->send(ct, str.c_str(), str.size());
     return true;
 }
 
-bool Telnetd::Impl::endSession(const SessionToken &session)
+bool Telnetd::Impl::endSession(const SessionToken &st)
 {
-    auto client = session_to_client_.at(session);
-    if (client.isNull())
+    auto ct = session_to_client_.at(st);
+    if (ct.isNull())
         return false;
 
-    client_to_session_.erase(client);
-    session_to_client_.erase(session);
-    sp_tcp_->disconnect(client);
+    client_to_session_.erase(ct);
+    session_to_client_.erase(st);
+    sp_tcp_->disconnect(ct);
     return true;
 }
 
-bool Telnetd::Impl::isValid(const SessionToken &session) const
+bool Telnetd::Impl::isValid(const SessionToken &st) const
 {
-    return session_to_client_.find(session) != session_to_client_.end();
+    return session_to_client_.find(st) != session_to_client_.end();
 }
 
-void Telnetd::Impl::onTcpConnected(const TcpServer::ClientToken &client)
+void Telnetd::Impl::onTcpConnected(const TcpServer::ClientToken &ct)
 {
-    cout << client.id() << " connected" << endl;
+    cout << ct.id() << " connected" << endl;
 
-    auto session = wp_terminal_->newSession(this);
-    client_to_session_[client] = session;
-    session_to_client_[session] = client;
+    auto st = wp_terminal_->newSession(this);
+    client_to_session_[ct] = st;
+    session_to_client_[st] = ct;
 
-    sendDont(client, kECHO);
-    sendCmd(client, kGA);
+    sendDont(ct, kECHO);
+    sendCmd(ct, kGA);
 }
 
-void Telnetd::Impl::onTcpDisconnected(const TcpServer::ClientToken &client)
+void Telnetd::Impl::onTcpDisconnected(const TcpServer::ClientToken &ct)
 {
-    cout << client.id() << " disconnected" << endl;
+    cout << ct.id() << " disconnected" << endl;
 
-    auto session = client_to_session_.at(client);
-    client_to_session_.erase(client);
-    session_to_client_.erase(session);
-    wp_terminal_->deleteSession(session);
+    auto st = client_to_session_.at(ct);
+    client_to_session_.erase(ct);
+    session_to_client_.erase(st);
+    wp_terminal_->deleteSession(st);
 }
 
-void Telnetd::Impl::onTcpReceived(const TcpServer::ClientToken &client, Buffer &buff)
+void Telnetd::Impl::onTcpReceived(const TcpServer::ClientToken &ct, Buffer &buff)
 {
     auto hex_str = string::RawDataToHexStr(buff.readableBegin(), buff.readableSize());
-    cout << "recv from " << client.id() << " recv " << buff.readableSize() << ": " << hex_str << endl;
+    cout << "recv from " << ct.id() << " recv " << buff.readableSize() << ": " << hex_str << endl;
 
-    auto session = client_to_session_.at(client);
+    auto st = client_to_session_.at(ct);
     std::string str(reinterpret_cast<const char*>(buff.readableBegin()), buff.readableSize());
-    wp_terminal_->input(session, str);
+    wp_terminal_->input(st, str);
 
     buff.hasReadAll();
 }
 
-bool Telnetd::Impl::send(const TcpServer::ClientToken &token, const void *data_ptr, size_t data_size)
+bool Telnetd::Impl::send(const TcpServer::ClientToken &ct, const void *data_ptr, size_t data_size)
 {
     auto hex_str = string::RawDataToHexStr(data_ptr, data_size);
-    cout << "send to " << token.id() << " size " << data_size << ": " << hex_str << endl;
+    cout << "send to " << ct.id() << " size " << data_size << ": " << hex_str << endl;
 
-    return sp_tcp_->send(token, data_ptr, data_size);
+    return sp_tcp_->send(ct, data_ptr, data_size);
 }
 
-void Telnetd::Impl::sendWill(const TcpServer::ClientToken &t, Opt o)
+void Telnetd::Impl::sendString(const TcpServer::ClientToken &ct, const std::string &str)
+{
+    send(ct, str.data(), str.size());
+}
+
+void Telnetd::Impl::sendWill(const TcpServer::ClientToken &ct, Opt o)
 {
     const uint8_t tmp[] = { Cmd::kIAC, Cmd::kWILL, o };
-    send(t, tmp, sizeof(tmp));
+    send(ct, tmp, sizeof(tmp));
 }
 
-void Telnetd::Impl::sendWont(const TcpServer::ClientToken &t, Opt o)
+void Telnetd::Impl::sendWont(const TcpServer::ClientToken &ct, Opt o)
 {
     const uint8_t tmp[] = { Cmd::kIAC, Cmd::kWONT, o };
-    send(t, tmp, sizeof(tmp));
+    send(ct, tmp, sizeof(tmp));
 }
 
-void Telnetd::Impl::sendDo(const TcpServer::ClientToken &t, Opt o)
+void Telnetd::Impl::sendDo(const TcpServer::ClientToken &ct, Opt o)
 {
     const uint8_t tmp[] = { Cmd::kIAC, Cmd::kDO, o };
-    send(t, tmp, sizeof(tmp));
+    send(ct, tmp, sizeof(tmp));
 }
 
-void Telnetd::Impl::sendDont(const TcpServer::ClientToken &t, Opt o)
+void Telnetd::Impl::sendDont(const TcpServer::ClientToken &ct, Opt o)
 {
     const uint8_t tmp[] = { Cmd::kIAC, Cmd::kDONT, o };
-    send(t, tmp, sizeof(tmp));
+    send(ct, tmp, sizeof(tmp));
 }
 
-void Telnetd::Impl::sendCmd(const TcpServer::ClientToken &t, Cmd c)
+void Telnetd::Impl::sendCmd(const TcpServer::ClientToken &ct, Cmd c)
 {
     const uint8_t tmp[] = { Cmd::kIAC, c };
-    send(t, tmp, sizeof(tmp));
+    send(ct, tmp, sizeof(tmp));
 }
 
-void Telnetd::Impl::sendSub(const TcpServer::ClientToken &t, Opt o, const uint8_t *p, size_t s)
+void Telnetd::Impl::sendSub(const TcpServer::ClientToken &ct, Opt o, const uint8_t *p, size_t s)
 {
     size_t size = s + 5;
     uint8_t tmp[size] = {
@@ -270,7 +286,42 @@ void Telnetd::Impl::sendSub(const TcpServer::ClientToken &t, Opt o, const uint8_
     tmp[size-2] = Cmd::kIAC;
     tmp[size-1] = Cmd::kSE;
 
-    send(t, tmp, size);
+    send(ct, tmp, size);
+}
+
+void Telnetd::Impl::onRecvString(const TcpServer::ClientToken &ct, const std::string &str)
+{
+    LogUndo();
+}
+
+void Telnetd::Impl::onRecvWill(const TcpServer::ClientToken &ct, Opt opt)
+{
+    LogUndo();
+}
+
+void Telnetd::Impl::onRecvWont(const TcpServer::ClientToken &ct, Opt opt)
+{
+    LogUndo();
+}
+
+void Telnetd::Impl::onRecvDo(const TcpServer::ClientToken &ct, Opt opt)
+{
+    LogUndo();
+}
+
+void Telnetd::Impl::onRecvDont(const TcpServer::ClientToken &ct, Opt opt)
+{
+    LogUndo();
+}
+
+void Telnetd::Impl::onRecvCmd(const TcpServer::ClientToken &ct, Cmd cmd)
+{
+    LogUndo();
+}
+
+void Telnetd::Impl::onRecvSub(const TcpServer::ClientToken &ct, Opt opt, const uint8_t *p, size_t s)
+{
+    LogUndo();
 }
 
 }
