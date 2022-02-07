04c0b211269be145c6f4a377b1a7908a50e2766a
Hevake <hevake_lcj@126.com>
2022-02-07 10:43:03 +0800

    实现了MoveUp与MoveDown历史记录回放的功能

--- terminal/impl/terminal.cpp
+++ terminal/impl/terminal.cpp
@@ -72,7 +72,6 @@ bool Terminal::Impl::onRecvString(const SessionToken &st, const std::string &str
     if (s == nullptr)
         return false;
 
-    LogTrace("%s", str.c_str());
     s->key_event_scanner_.start();
     for (char c : str) {
         auto status = s->key_event_scanner_.next(c);
@@ -175,8 +174,6 @@ void Terminal::Impl::onChar(SessionImpl *s, char ch)
     ss  << s->curr_input.substr(s->cursor)
         << std::string((s->curr_input.size() - s->cursor), '\b');
     s->send(ss.str());
-
-    LogTrace("s->curr_input: %s", s->curr_input.c_str());
 }
 
 void Terminal::Impl::onEnterKey(SessionImpl *s)
@@ -198,6 +195,7 @@ void Terminal::Impl::onEnterKey(SessionImpl *s)
         s->cursor = s->curr_input.size();
         s->send(s->curr_input);
     }
+    s->history_index = 0;
 }
 
 void Terminal::Impl::onBackspaceKey(SessionImpl *s)
@@ -216,8 +214,6 @@ void Terminal::Impl::onBackspaceKey(SessionImpl *s)
     ss  << '\b' << s->curr_input.substr(s->cursor) << ' '
         << std::string((s->curr_input.size() - s->cursor + 1), '\b');
     s->send(ss.str());
-
-    LogTrace("s->curr_input: %s", s->curr_input.c_str());
 }
 
 void Terminal::Impl::onDeleteKey(SessionImpl *s)
@@ -240,12 +236,35 @@ void Terminal::Impl::onTabKey(SessionImpl *s)
 
 void Terminal::Impl::onMoveUpKey(SessionImpl *s)
 {
-    LogUndo();
+    if (s->history_index == s->history.size())
+        return;
+
+    while (s->cursor--)
+        s->send("\b \b");
+
+    s->history_index++;
+    s->curr_input = s->history[s->history.size() - s->history_index];
+    s->cursor = s->curr_input.size();
+    s->send(s->curr_input);
 }
 
 void Terminal::Impl::onMoveDownKey(SessionImpl *s)
 {
-    LogUndo();
+    if (s->history_index == 0)
+        return;
+
+    while (s->cursor--)
+        s->send("\b \b");
+
+    s->history_index--;
+    if (s->history_index > 0) {
+        s->curr_input = s->history[s->history.size() - s->history_index];
+        s->cursor = s->curr_input.size();
+    } else {
+        s->curr_input.clear();
+        s->cursor = 0;
+    }
+    s->send(s->curr_input);
 }
 
 void Terminal::Impl::onMoveLeftKey(SessionImpl *s)
