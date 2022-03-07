a81d5a30ba16b82306553fb0300d0b5564a64d2b
海卫哥 <hevake@126.com>
2022-03-07 00:23:26 +0000

    update base/json_fwd.h.

--- base/json_fwd.h
+++ base/json_fwd.h
@@ -4,7 +4,8 @@
 #include <nlohmann/json_fwd.hpp>
 
 //! 如果找到不该文件，则从 github 下载：
-//! https://raw.githubusercontent.com/nlohmann/json/v3.10.4/include/nlohmann/json_fwd.hpp
+//! https://github.com/nlohmann/json/blob/v3.10.4/include/nlohmann/json_fwd.hpp
+//! 下载链接：https://raw.githubusercontent.com/nlohmann/json/v3.10.4/include/nlohmann/json_fwd.hpp
 //! 放置到 /usr/local/include/nlohmann/ 目录下
 
 namespace tbox {
