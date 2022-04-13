039abb25d838d16a8cbcf9434a31d3f38d032c6f
Hevake <hevake_lcj@126.com>
2022-04-13 22:17:41 +0800

    添加各类日志的性能测试用例

--- log/async_channel_test.cpp
+++ log/async_channel_test.cpp
@@ -124,3 +124,33 @@ TEST(AsyncChannel, TimeCast2)
     chrono::microseconds time_span = chrono::duration_cast<chrono::microseconds>(end_ts - start_ts);
     cout << "timecost: " << time_span.count() << " us" << endl;
 }
+
+#include <tbox/event/loop.h>
+using namespace tbox::event;
+
+TEST(AsyncChannel, Benchmark)
+{
+    TestAsyncChannel ch;
+    ch.initialize(TestAsyncChannel::Config());
+    ch.enable();
+    std::string tmp(30, 'x');
+
+    auto sp_loop = Loop::New();
+
+    int counter = 0;
+    function<void()> func = [&] {
+        for (int i = 0; i < 100; ++i)
+            LogInfo("%d %s", i, tmp.c_str());
+        sp_loop->runInLoop(func);
+        counter += 100;
+    };
+    sp_loop->runInLoop(func);
+
+    sp_loop->exitLoop(chrono::seconds(10));
+    sp_loop->runLoop();
+
+    delete sp_loop;
+    cout << "count in sec: " << counter/10 << endl;
+    ch.cleanup();
+}
+
--- log/file_async_channel_test.cpp
+++ log/file_async_channel_test.cpp
@@ -114,3 +114,32 @@ TEST(FileAsyncChannel, ParamNormalize)
     LogInfo("Test LogPath");
     ch.cleanup();
 }
+
+#include <tbox/event/loop.h>
+using namespace tbox::event;
+
+TEST(FileAsyncChannel, Benchmark)
+{
+    FileAsyncChannel ch;
+    ch.initialize("test", "/tmp/tbox");
+    ch.enable();
+    std::string tmp(30, 'x');
+
+    auto sp_loop = Loop::New();
+
+    int counter = 0;
+    function<void()> func = [&] {
+        for (int i = 0; i < 100; ++i)
+            LogInfo("%d %s", i, tmp.c_str());
+        sp_loop->runInLoop(func);
+        counter += 100;
+    };
+    sp_loop->runInLoop(func);
+
+    sp_loop->exitLoop(chrono::seconds(10));
+    sp_loop->runLoop();
+
+    delete sp_loop;
+    cout << "count in sec: " << counter/10 << endl;
+}
+
