95f12acd934148dfc07443276f703c2312edf67b
Hevake <hevake_lcj@126.com>
2022-04-28 08:19:34 +0800

    单独实现了TimeoutMonitor模块，用于管理超时

---
+++ util/timeout_monitor.h
@@ -0,0 +1,51 @@
+#ifndef TBOX_UTIL_TIMEOUT_MONITOR_H_20220428
+#define TBOX_UTIL_TIMEOUT_MONITOR_H_20220428
+
+#include <tbox/base/cabinet_token.h>
+#include <tbox/event/loop.h>
+
+namespace tbox {
+namespace util {
+
+/**
+ * Token的超时监控器
+ *
+ * 该类通常配合RequestPool或cabinet::Cabinet使用
+ * 用于管理请求的超时机制
+ */
+class TimeoutMonitor {
+  public:
+    using Token = cabinet::Token;
+    using Duration = std::chrono::milliseconds;
+    using Callback = std::function<void(const Token&)>;
+
+  public:
+    explicit TimeoutMonitor(event::Loop *wp_loop);
+    virtual ~TimeoutMonitor();
+
+    /**
+     * \brief   初始化
+     *
+     * \param   check_interval  指定检查时间间隔
+     * \param   check_times     指定检查次数
+     * \param   timeout_action  指定超时的动作
+     *
+     * \return  bool    成功与否，通常都不会失败
+     * \note    尽要权衡，不要让check_times太大
+     */
+    bool initialize(const Duration &check_interval, int check_times);
+    void setCallback(const Callback &cb);
+
+    void add(const Token &token);
+
+    void cleanup();
+
+  private:
+    class Impl;
+    Impl *impl_;
+};
+
+}
+}
+
+#endif //TBOX_UTIL_TIMEOUT_MONITOR_H_20220428
---
+++ util/timeout_monitor_test.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include "timeout_monitor.h"
+#include <tbox/base/scope_exit.hpp>
+
+namespace tbox {
+namespace util {
+namespace {
+
+using namespace event;
+using namespace std::chrono;
+
+TEST(TimeoutMonitor, Basic)
+{
+    auto sp_loop = Loop::New();
+    SetScopeExitAction([=] {delete sp_loop;});
+
+    TimeoutMonitor tm(sp_loop);
+    tm.initialize(milliseconds(100), 10);
+
+    auto start_time = steady_clock::now();
+
+    cabinet::Token pushed_token(100, 1);
+    bool run = false;
+    tm.setCallback([&] (const cabinet::Token &token) {
+        EXPECT_EQ(token, pushed_token);
+
+        auto d = steady_clock::now() - start_time;
+        EXPECT_GT(d, milliseconds(900));
+        EXPECT_LT(d, milliseconds(1100));
+        run = true;
+    });
+
+    tm.add(pushed_token);
+    sp_loop->exitLoop(milliseconds(1200));
+    sp_loop->runLoop();
+
+    EXPECT_TRUE(run);
+}
+
+}
+}
+}
