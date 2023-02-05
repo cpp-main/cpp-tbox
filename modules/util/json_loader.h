2937ee137345e4c7f8c86a12b3c5ba44e05cf5bb
Hevake <hevake@126.com>
2023-02-05 19:52:36 +0800

    tidy:为JsonLoader添加使用说明

--- modules/util/json_loader.h
+++ modules/util/json_loader.h
@@ -1,6 +1,66 @@
 #ifndef TBOX_UTIL_JSON_LOADER_H_20221224
 #define TBOX_UTIL_JSON_LOADER_H_20221224
 
+/**
+ * 本文定义了DeepLoader，是一种JSON文件加载器。
+ *
+ * 它与util/json.h中实现的Load()不同的是：它会检查JSON数据中有没有"__include__"关键对象。
+ * 如果存在，它会进一步去解析其中的描述，并将描述中所指向的其它JSON文件也加载进来，合并到本
+ * 对象中。
+ *
+ * "__include__" 对象有两种格式："xxxxx" 与 ["xxxxx", "yyyyy", ...]
+ * 前者是一个导入字串，表示包含导入单个JSON文件。后者是一个列表，表示要导入多个JSON文件。
+ *
+ * 导入字串格式："<JSON文件名>[=>结点ID]"
+ * 如："sub.json", "sub.json => abc"，前者表示将 sub.json 中的内容导入到进当前结点，后者表示将
+ * sub.json 中的内容导入到 "abc" 这个结点上。
+ *
+ * 以下为要导入的JSON文件：
+ * |- main.json
+ * |- common.json
+ * |- sub3.json
+ * `- sub
+ *    |- sub1.json
+ *    `- sub2.json
+ *
+ * 内容分别为：
+ * main.json
+ * {
+ *   "main.a": 1,
+ *   "__include__":["sub/sub1.json => sub1", "common.json"],
+ * }
+ *
+ * sub/sub1.json
+ * {
+ *   "sub1.a": 1,
+ *   "__include__":"sub2.json => sub2"
+ * }
+ *
+ * common.json
+ * {
+ *   "common.a": 2
+ * }
+ *
+ * sub/sub2.json
+ * [1, 2, 3, { "__include__": "../sub3.json" }]
+ *
+ * sub3.json
+ * {
+ *   "sub3.a": 4
+ * }
+ *
+ * 加载后结果为：
+ * {
+ *   "common.a": 2,
+ *   "main.a" : 1,
+ *   "sub1": {
+ *      "sub1.a": 1,
+ *      "sub2": [1, 2, 3, { "sub3.a": 4 }]
+ *   },
+ * }
+ *
+ */
+
 #include <tbox/base/json_fwd.h>
 
 namespace tbox {
