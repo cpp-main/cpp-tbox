aedbb18892adb4475f81d698a9119a10f91758d8
Hevake <hevake@126.com>
2023-02-11 23:43:39 +0800

    feat(eventx): 实现了WorkThread类

---
+++ modules/eventx/work_thread.h
@@ -0,0 +1,92 @@
+#ifndef TBOX_WORK_THREAD_H
+#define TBOX_WORK_THREAD_H
+
+#include <limits>
+#include <functional>
+#include <array>
+#include <tbox/event/forward.h>
+#include <tbox/base/cabinet_token.h>
+
+namespace tbox {
+namespace eventx {
+
+/**
+ * 工作线程
+ *
+ * 是ThreadPool的精简版，单线程+无任务等级
+ */
+class WorkThread {
+  public:
+    using TaskToken = cabinet::Token;
+
+    /**
+     * 构造函数
+     *
+     * \param main_loop         主线程的Loop对象指针
+     */
+    explicit WorkThread(event::Loop *main_loop = nullptr);
+    virtual ~WorkThread();
+
+    using NonReturnFunc = std::function<void ()>;
+
+    /**
+     * 使用worker线程执行某个函数
+     *
+     * \param backend_task      让worker线程执行的函数对象
+     *
+     * \return TaskToken        任务Token
+     */
+    TaskToken execute(const NonReturnFunc &backend_task);
+
+    /**
+     * 使用worker线程执行某个函数，并在完成之后在主线程执行指定的回调函数
+     *
+     * \param backend_task      让worker线程执行的函数对象
+     * \param main_cb           任务完成后，由主线程执行的回调函数对象
+     *
+     * \return TaskToken        任务Token
+     */
+    TaskToken execute(const NonReturnFunc &backend_task, const NonReturnFunc &main_cb, event::Loop *main_loop = nullptr);
+
+    enum class TaskStatus {
+        kWaiting,   //! 等待中
+        kExecuting, //! 执行中
+        kNotFound   //! 未找到（可能已完成）
+    };
+
+    //! 获取任务的状态
+    TaskStatus getTaskStatus(TaskToken task_token) const;
+
+    /**
+     * 取消任务
+     *
+     * \return task_token       任务Token
+     *
+     * \return  int     0: 成功
+     *                  1: 没有找到该任务（已执行）
+     *                  2: 该任务正在执行
+     */
+    int cancel(TaskToken task_token);
+
+    /**
+     * 清理资源，并等待所有的worker线程结束
+     */
+    void cleanup();
+
+  protected:
+    void threadProc();
+
+    bool shouldThreadExitWaiting() const;   //! 判定子线程是否需要退出条件变量的wait()函数
+
+    struct Task;
+    Task* popOneTask(); //! 取出一个优先级最高的任务
+
+  private:
+    struct Data;
+    Data *d_ = nullptr;
+};
+
+}
+}
+
+#endif //TBOX_WORK_THREAD_H
---
+++ modules/eventx/work_thread_test.cpp
@@ -0,0 +1,116 @@
+#include <thread>
+#include <gtest/gtest.h>
+
+#include <tbox/base/log.h>
+#include <tbox/event/loop.h>
+#include <tbox/event/timer_event.h>
+
+#include "work_thread.h"
+
+using namespace std;
+using namespace tbox::event;
+using namespace tbox::eventx;
+
+namespace {
+
+auto backend_func = \
+        [](int id) {
+            LogDbg("<<task %d run in back", id);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            LogDbg("task %d run in back>>", id);
+        };
+
+WorkThread::TaskToken null_task_token;
+
+/**
+ * 测试是否所有的任务都能被执行
+ */
+TEST(WorkThread, all_task_executed_without_loop) {
+    WorkThread *tp = new WorkThread;
+
+    int count = 0;
+    for (int i = 0; i < 3; ++i) {
+        tp->execute([&] {++count;});
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    delete tp;
+    EXPECT_EQ(count, 3);
+}
+
+/**
+ * 测试是否所有的任务都能被执行
+ */
+TEST(WorkThread, all_task_executed_with_loop) {
+    Loop *loop = Loop::New();
+
+    WorkThread *tp = new WorkThread(loop);
+
+    int count = 0;
+    for (int i = 0; i < 3; ++i) {
+        tp->execute([]{}, [&]{++count;});
+    }
+
+    LogDbg("run in main");
+    loop->exitLoop(std::chrono::milliseconds(1));
+    loop->runLoop();
+
+    delete tp;
+    delete loop;
+
+    EXPECT_EQ(count, 3);
+}
+
+/**
+ * 不等其完成工作就退出主线程
+ * 主要是检查有没有内存泄漏
+ */
+TEST(WorkThread, exit_before_finish) {
+    Loop *loop = Loop::New();
+
+    WorkThread *tp = new WorkThread(loop);
+
+    for (int i = 0; i < 3; ++i) {
+        ASSERT_NE(tp->execute(std::bind(backend_func, i)), null_task_token);
+    }
+
+    LogDbg("run in main");
+    loop->exitLoop(std::chrono::seconds(1));
+    loop->runLoop();
+
+    delete tp;
+    delete loop;
+}
+
+/**
+ * 取消任务
+ *
+ * 创建三个任务。在1.5秒时全部取消。
+ */
+TEST(WorkThread, cancel_task) {
+    Loop *loop = Loop::New();
+
+    WorkThread *tp = new WorkThread(loop);
+
+    vector<WorkThread::TaskToken> task_ids;
+    for (int i = 0; i < 3; ++i) {
+        auto token = tp->execute(std::bind(backend_func, i));
+        task_ids.push_back(token);
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+
+    EXPECT_EQ(tp->cancel(task_ids[0]), 1);  //! 第一个任务已完成
+    EXPECT_EQ(tp->cancel(task_ids[1]), 2);  //! 第二个任务正在执行
+    EXPECT_EQ(tp->cancel(task_ids[2]), 0);  //! 第三个任务可正常取消
+    WorkThread::TaskToken invalid_token(100, 1);
+    EXPECT_EQ(tp->cancel(invalid_token), 1);  //! 任务不存在
+
+    loop->exitLoop(std::chrono::seconds(4));
+    loop->runLoop();
+
+    delete tp;
+    delete loop;
+}
+
+}
