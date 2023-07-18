74205336bd5fd926a1dd9c0ed035efa4a7a6df67
Hevake <hevake@126.com>
2023-07-18 23:30:18 +0800

    fix:1.5.9, 去除对libevent与libev的依赖，并确保在无libevent与libev库的情况下通过全编译

--- build_env.mk
+++ build_env.mk
@@ -1,13 +1,13 @@
 export TOP_DIR := $(PWD)
 
+export TOOLCHAIN_BIN_PREFIX ?=
+export BUILD_DIR ?= $(TOP_DIR)/.build
 export STAGING_DIR ?= $(TOP_DIR)/.staging
+export INSTALL_DIR ?= $(TOP_DIR)/.install
+
 export STAGING_INCLUDE := $(STAGING_DIR)/include
 export STAGING_LIB := $(STAGING_DIR)/lib
-
-export INSTALL_DIR := $(TOP_DIR)/.install
 export INSTALL_LIB := $(INSTALL_DIR)/lib
-
-export BUILD_DIR := $(TOP_DIR)/.build
 
 CCFLAGS := -I$(STAGING_INCLUDE)
 
@@ -15,7 +15,6 @@ export CFLAGS := $(CCFLAGS) -std=c99
 export CXXFLAGS := $(CCFLAGS) -std=c++11
 export LDFLAGS := -L$(STAGING_LIB) -L$(INSTALL_LIB)
 
-TOOLCHAIN_BIN_PREFIX :=
 export AR := $(TOOLCHAIN_BIN_PREFIX)ar
 export AS := $(TOOLCHAIN_BIN_PREFIX)as
 export CXX := $(TOOLCHAIN_BIN_PREFIX)g++
